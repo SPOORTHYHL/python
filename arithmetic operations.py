474271ded24208f03042afa9c278aef5bb9198bf
SPOORTHYHL <128689366+SPOORTHYHL@users.noreply.github.com>
2023-03-25 22:41:15 +0530

    Add files via upload

---
+++ arithmetic operations.py
@@ -0,0 +1,17 @@
+# A program to perform arithmetic operations
+
+# Get input from user
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+# Perform arithmetic operations
+addition = num1 + num2
+subtraction = num1 - num2
+multiplication = num1 * num2
+division = num1 / num2
+
+# Display results
+print("Addition of", num1, "and", num2, "is:", addition)
+print("Subtraction of", num1, "and", num2, "is:", subtraction)
+print("Multiplication of", num1, "and", num2, "is:", multiplication)
+print("Division of", num1, "and", num2, "is:", division)
